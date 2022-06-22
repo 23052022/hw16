02682bc880185c479ac305bfad408d765eccee8a
23052022 <golovataya.olya@gmail.com>
2022-06-22 17:29:32 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+n = int(input("enter number:"))
+x = 2
+result = 1
+for i in range (n):
+    result *=x
+print(result)
+
+n = int(input("enter number:"))
+x = 2
+a = x ** n
+def result(n):
+    return 1 if i > n else a == x**n
+print(a)
